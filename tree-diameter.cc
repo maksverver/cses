f5621eea90466dea9e1343cd1cb646770d458a2b
Maks Verver <maks@verver.ch>
2023-10-13 16:34:44 +0200

    Add a different solution to Tree Diameter (simpler, but harder to prove correct).

--- tree-diameter.cc
+++ tree-diameter.cc
@@ -1,100 +1,56 @@
 // Tree Diameter
 // https://cses.fi/problemset/task/1131/
+//
+// The diameter of a graph is defined as the maximum length of a shortest path
+// between any pair of vertices. To calculate the diameter of a tree:
+//
+//  - Start from an arbitrary vertex u.
+//  - Let v be a vertex at maximum distance from u.
+//  - Let w be a vertex at maximum distance from v.
+//
+// Then the distance from v to w is maximal. The farthest vertices can be found
+// easily with a breadth-first search (or, if stack space is not a concern, with
+// a depth-first search).
+//
+// This feels intuitive but it is not trivial to prove formally.
+// Some pointers here: https://codeforces.com/blog/entry/60440
+//
+// See also tree-diameter-2.cc for a version uses dynamic programming (DP) on
+// the tree instead. This is more complicated to implement, but easier to prove
+// correct, and runs with the same time complexity.
 
 #include <bits/stdc++.h>
 
-#if __has_include("dump.h")
-#include "dump.h"
-#else
-#define DUMP(...)
-#endif
-
 using namespace std;
-
-#define FOR(i,a,b) for (int i = int(a), _end_##i = int(b); i < _end_##i; ++i)
-#define REP(i,n) FOR(i,0,n)
-
-template<class T>
-ostream &operator<<(ostream &os, const vector<T> &v) {
-  REP(i, v.size()) {
-    if (i > 0) os << ' ';
-    os << v[i];
-  }
-  return os;
-}
 
 int N;
 vector<vector<int>> adj;
 
-// subtree_depth[v] = depth of subtree rooted at v, but excluding the path
-// to the top. For example:
+// Searches for a vertex at maximum distance of `start` and returns a
+// pair {vertex, distance}.
 //
-//      1        |
-//     / \       |
-//    2   3      |
-//   / \         |
-//  4   5        |
-//
-// subtree_depth[1] = 2
-// subtree_depth[2] = 1
-// subtree_depth[4] = 0
-vector<int> subtree_depth;
-
-// depth[v] = depth of tree rooted at v
-//
-// In the above example:
-//  depth[1] = 2
-//  depth[2] = 2
-//  depth[4] = 3
-vector<int> depth;
-
-int CalcSubtreeDepth(int v, int u) {
-  int res = 0;
-  for (int w : adj[v]) if (w != u) res = std::max(res, 1 + CalcSubtreeDepth(w, v));
-  subtree_depth[v] = res;
-  return res;
-}
-
-// Calculate depth of subtree rooted at 'v', coming from parent `u`.
-// `d` is the maximum length of the path going up to the parent.
-//
-// The idea is that if we call two children with the deepest subtrees
-// w1 and w2, with depths d1 >= d2.
-//
-//           (u)
-//            | d
-//           (v)
-//       d1 / | \ d2
-//       (w1) |  (w2)
-//           (w)
-//
-void CalcDepth(int v, int u, int d) {
-  int w1 = -1, d1 = 0, d2 = 0;
-  for (int w : adj[v]) if (w != u) {
-    int d = subtree_depth[w] + 1;
-    if (d > d1) {
-      d2 = d1;
-      w1 = w;
-      d1 = d;
-    } else if (d > d2) {
-      d2 = d;
+// This just does a single-source breadth-first search (BFS), which works for
+// arbitrary graphs, not just trees!
+pair<int, int> FindFarthestVertex(int start) {
+  vector<int> dist(N, -1);
+  dist[start] = 0;
+  vector<int> todo;
+  todo.push_back(start);
+  for (size_t i = 0; i != todo.size(); ++i) {
+    int v = todo[i];
+    for (int w : adj[v]) if (dist[w] == -1) {
+      dist[w] = dist[v] + 1;
+      todo.push_back(w);
     }
   }
-  depth[v] = std::max(d, d1);
-  for (int w : adj[v]) if (w != u) {
-    // Note the new value of `d` is the longest path going either up
-    // (w-v-u-...) or (w-v-w1-...) or, if w == w1, (w-v-w2-....)
-    CalcDepth(w, v, std::max(d, w1 == w ? d2 : d1) + 1);
-  }
+  int v = todo.back();
+  return {v, dist[v]};
 }
 
 int Solve() {
-  subtree_depth = vector<int>(N);
-  depth = vector<int>(N);
-  CalcSubtreeDepth(0, 0);
-  CalcDepth(0, 0, 0);
-
-  return *max_element(depth.begin(), depth.end());
+  int v = FindFarthestVertex(0).first;
+  int d = FindFarthestVertex(v).second;
+  return d;
 }
 
 int main() {
@@ -104,7 +60,7 @@ int main() {
   N = 0;
   cin >> N;
   adj = vector<vector<int>>(N);
-  REP(n, N - 1) {
+  for (int e = 0; e < N - 1; ++e) {
     int u = 0, v = 0;
     cin >> u >> v;
     --u, --v;
@@ -112,5 +68,6 @@ int main() {
     adj[u].push_back(v);
     adj[v].push_back(u);
   }
-  cout << Solve() << '\n';
+
+  cout << Solve() << endl;
 }
